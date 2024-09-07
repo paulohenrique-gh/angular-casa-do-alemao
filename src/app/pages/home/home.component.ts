f51df145a9b242810f739f0c9d5eed72140b7a5c
Paulo Henrique <ph.1928@hotmail.com>
2024-09-07 15:12:46 -0300

    refactor: home component

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CountryInfo } from '../../../data';
 
 @Component({
   selector: 'app-home',
@@ -8,5 +9,4 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-
 }
