522aad6440285a3bce4981264b008cff37f5e32c
Paulo Henrique <ph.1928@hotmail.com>
2024-09-08 15:33:54 -0300

    feat: create states route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { StatesComponent } from './pages/states/states.component';
 
 export const routes: Routes = [
     {
         path: '',
         component: HomeComponent
+    },
+    {
+        path: 'states',
+        component: StatesComponent
     }
 ];
