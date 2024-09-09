686e2ff63255c1677646f0468f3dc2a9494050bc
Paulo Henrique <ph.1928@hotmail.com>
2024-09-08 22:43:20 -0300

    refactor: account for undefined string in SplitParagraphPipe#transform

--- src/app/pipes/split-paragraph.pipe.ts
+++ src/app/pipes/split-paragraph.pipe.ts
@@ -1,11 +1,18 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-    name: 'splitParagraph',
-    standalone: true
+  name: 'splitParagraph',
+  standalone: true,
 })
 export class SplitParagraphPipe implements PipeTransform {
-  transform(value: string): string[] {
-    return value.split('\n');
+  transform(value: string | undefined): string[] | [] {
+    if (value) {
+      return value
+        .split('\n')
+        .filter((paragraph) => paragraph !== '')
+        .map((paragraph) => paragraph.trim());
+    }
+
+    return [];
   }
 }
