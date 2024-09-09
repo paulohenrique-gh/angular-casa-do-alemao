8d053e1f17b7c6f4b304dace4e17bd4717ceaf8b
Paulo Henrique <ph.1928@hotmail.com>
2024-09-08 22:42:06 -0300

    chore: remove unused import in CountryInfoService

--- src/app/services/country-info.service.ts
+++ src/app/services/country-info.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { germanStates, State } from '../../data';
-import { BehaviorSubject, Observable, Subject } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
