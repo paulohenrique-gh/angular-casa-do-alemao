b67ca9763475787d6e16dd741792db97a1542fce
Paulo Henrique <ph.1928@hotmail.com>
2024-09-09 22:08:19 -0300

    refactor: replace galery component header with new header component

--- src/app/pages/gallery/gallery.component.ts
+++ src/app/pages/gallery/gallery.component.ts
@@ -3,11 +3,12 @@ import { GalleryImage } from '../../../data';
 import { CountryInfoService } from '../../services/country-info.service';
 import { Observable } from 'rxjs';
 import { AsyncPipe } from '@angular/common';
+import { HeaderComponent } from "../../components/header/header.component";
 
 @Component({
   selector: 'app-gallery',
   standalone: true,
-  imports: [AsyncPipe],
+  imports: [AsyncPipe, HeaderComponent],
   templateUrl: './gallery.component.html',
   styleUrl: './gallery.component.scss'
 })
