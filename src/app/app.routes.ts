ea4868ec8a4cb42d89738251dd3da3476f730039
Paulo Henrique <ph.1928@hotmail.com>
2024-09-09 22:06:35 -0300

    feat: create route for articles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,18 +2,23 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { StatesComponent } from './pages/states/states.component';
 import { GalleryComponent } from './pages/gallery/gallery.component';
+import { ArticlesComponent } from './pages/articles/articles.component';
 
 export const routes: Routes = [
-    {
-        path: '',
-        component: HomeComponent
-    },
-    {
-        path: 'states',
-        component: StatesComponent
-    },
-    {
-        path: 'gallery',
-        component: GalleryComponent
-    }
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'states',
+    component: StatesComponent,
+  },
+  {
+    path: 'gallery',
+    component: GalleryComponent,
+  },
+  {
+    path: 'articles',
+    component: ArticlesComponent,
+  },
 ];
