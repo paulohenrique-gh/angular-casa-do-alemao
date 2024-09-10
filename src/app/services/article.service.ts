8ef7b312f07c92876f0c61529dd3720f5ae3ffaf
Paulo Henrique <ph.1928@hotmail.com>
2024-09-10 16:17:55 -0300

    refactor: Update ArticleService to use HttpClient for API requests

--- src/app/services/article.service.ts
+++ src/app/services/article.service.ts
@@ -1,16 +1,27 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable } from 'rxjs';
-import { Article, articles } from '../../data';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Article } from '../models/article';
+import { Comment } from '../models/comment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ArticleService {
-  private articlesSubject: BehaviorSubject<Article[]> = new BehaviorSubject<Article[]>(articles)
+  private articlesBaseUrl: string = 'http://localhost:3000/articles';
+  private commentsBaseUrl: string = 'http://localhost:3000/comments'
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
 
   getArticles(): Observable<Article[]> {
-    return this.articlesSubject.asObservable();
+    return this.httpClient.get<Article[]>(this.articlesBaseUrl);
+  }
+
+  getArticleById(articleId: string): Observable<Article> {
+    return this.httpClient.get<Article>(`${this.articlesBaseUrl}/${articleId}`);
+  }
+
+  getArticleComments(articleId: string): Observable<Comment[]> {
+    return this.httpClient.get<Comment[]>(`${this.commentsBaseUrl}?articleId=${articleId}`);
   }
 }
