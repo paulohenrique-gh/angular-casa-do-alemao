894e7e232e15b32d0bbf9f42c49890e1d466aa7c
Paulo Henrique <ph.1928@hotmail.com>
2024-09-09 22:10:22 -0300

    feat: add content to articles template and methods in component ts file

--- src/app/pages/articles/articles.component.ts
+++ src/app/pages/articles/articles.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Article } from '../../../data';
+import { ArticleService } from '../../services/article.service';
+import { AsyncPipe, DatePipe } from '@angular/common';
+import { HeaderComponent } from "../../components/header/header.component";
 
 @Component({
   selector: 'app-articles',
   standalone: true,
-  imports: [],
+  imports: [AsyncPipe, DatePipe, HeaderComponent],
   templateUrl: './articles.component.html',
   styleUrl: './articles.component.scss'
 })
-export class ArticlesComponent {
+export class ArticlesComponent implements OnInit {
+  articles$!: Observable<Article[]>
 
+  constructor(private articleService: ArticleService) {}
+  
+  ngOnInit(): void {
+    this.articles$ = this.articleService.getArticles();
+  }
+
+  loadDefaultImage(event: any): void {
+    event.target.src = 'https://img.freepik.com/fotos-gratis/uma-pequena-bandeira-da-alemanha-na-cidade-borrada_485709-15.jpg'
+  }
 }
