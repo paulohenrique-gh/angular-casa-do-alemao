cf4ee650f1faf3556c48df226cf736e98cabaa02
Paulo Henrique <ph.1928@hotmail.com>
2024-09-11 14:39:35 -0300

    doc: add documentation for header component

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,6 +1,21 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 
+
+/**
+ * HeaderComponent is a standalone Angular component that represents the header section of the application.
+ * The appropriate content to be passed is a button for user actions.
+ * 
+ * @selector 'app-header'
+ * @standalone true
+ * @imports [CommonModule]
+ * @templateUrl './header.component.html'
+ * @styleUrl './header.component.scss'
+ * 
+ * @property {string} header - The header text to be displayed. This input is required.
+ * @property {'xl' | 'lg'} size - The size of the header. It can be either 'xl' (extra large) or 'lg' (large). The default value is 'xl'.
+ * 
+ */
 @Component({
   selector: 'app-header',
   standalone: true,
