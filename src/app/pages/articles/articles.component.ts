fcbc35023e34a3801a36e63477420e38502618b9
Paulo Henrique <ph.1928@hotmail.com>
2024-09-11 14:40:02 -0300

    feat: add button to articles page for creating new article

--- src/app/pages/articles/articles.component.ts
+++ src/app/pages/articles/articles.component.ts
@@ -5,17 +5,20 @@ import { AsyncPipe, DatePipe } from '@angular/common';
 import { HeaderComponent } from "../../components/header/header.component";
 import { RouterModule } from '@angular/router';
 import { Article } from '../../models/article';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-articles',
   standalone: true,
-  imports: [AsyncPipe, DatePipe, HeaderComponent, RouterModule],
+  imports: [AsyncPipe, DatePipe, HeaderComponent, RouterModule, FontAwesomeModule],
   templateUrl: './articles.component.html',
   styleUrl: './articles.component.scss',
   host: {class: 'w-full'}
 })
 export class ArticlesComponent implements OnInit {
   articles$!: Observable<Article[]>
+  faPlus = faPlus;
 
   constructor(private articleService: ArticleService) {}
   
