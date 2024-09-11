93882a8ae45b8082e1a7e56bc204558c96e80a60
Paulo Henrique <ph.1928@hotmail.com>
2024-09-11 16:20:30 -0300

    feat: create new article component

---
+++ src/app/components/new-article/new-article.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-article',
+  standalone: true,
+  imports: [],
+  templateUrl: './new-article.component.html',
+  styleUrl: './new-article.component.scss'
+})
+export class NewArticleComponent {
+
+}
