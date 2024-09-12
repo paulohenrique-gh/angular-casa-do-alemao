24967280153ef7603b99f5a2d96108fa9dec018a
Paulo Henrique <ph.1928@hotmail.com>
2024-09-12 11:57:16 -0300

    feat: article form

--- src/app/components/new-article/new-article.component.ts
+++ src/app/components/new-article/new-article.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { ArticleFormData } from '../../models/article-form-data';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-new-article',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './new-article.component.html',
   styleUrl: './new-article.component.scss'
 })
 export class NewArticleComponent {
+  @Output() modalClosed = new EventEmitter();
+  article: ArticleFormData = {
+    title: '',
+    imageUrl: '',
+    previewText: '',
+    content: ''
+  }
 
+  closeModal(): void {
+    this.modalClosed.emit();
+  }
+
+  submit(): void {
+    console.log(this.article)
+  }
 }
