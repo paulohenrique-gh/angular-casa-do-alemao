f4090ab2f1fee91e9958f63302da72e69d9b968f
Paulo Henrique <ph.1928@hotmail.com>
2024-09-13 12:02:08 -0300

    refactor: move snackBar handling to articles component

--- src/app/components/article-form/article-form.component.ts
+++ src/app/components/article-form/article-form.component.ts
@@ -7,7 +7,6 @@ import { faExclamationTriangle } from '@fortawesome/free-solid-svg-icons/faExcla
 import { AuthService } from '../../services/auth.service';
 import { ArticleService } from '../../services/article.service';
 import { Article } from '../../models/article';
-import { MatSnackBar } from '@angular/material/snack-bar';
 import { faCross } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
@@ -19,6 +18,7 @@ import { faCross } from '@fortawesome/free-solid-svg-icons';
 })
 export class ArticleFormComponent {
   @Output() modalClosed = new EventEmitter();
+  @Output() formSubmitted = new EventEmitter();
   @ViewChild('articleForm') articleForm!: NgForm;
 
   article: ArticleFormData = {
@@ -33,7 +33,6 @@ export class ArticleFormComponent {
   constructor(
     private authService: AuthService,
     private articleService: ArticleService,
-    private snackBar: MatSnackBar,
   ) {}
 
   closeModal(): void {
@@ -43,31 +42,19 @@ export class ArticleFormComponent {
 
   submit(): void {
     const currentUser = this.authService.getCurrentUser();
-    if (currentUser) {
+    if (currentUser && currentUser.role === 'editor') {
       const newArticle: Article = {
         ...this.article,
         publicationDate: new Date(),
         user: currentUser,
       };
       this.articleService.saveArticle(newArticle).subscribe({
-        next: (data) => {
-          console.log(data);
-          this.modalClosed.emit();
-          this.openSnackBar();
+        next: () => {
+          this.formSubmitted.emit();
+          this.articleForm.reset();
         },
-        error: (error) => console.log(error)
+        error: (error) => alert(error)
       });
     }
-  }
-
-  openSnackBar(): void {
-    this.snackBar.open('Artigo criado com sucesso', '', {
-      horizontalPosition: 'center',
-      verticalPosition: 'bottom',
-      panelClass: [
-        'snackbar-custom',
-      ],
-      duration: 3000
-    });
   }
 }
