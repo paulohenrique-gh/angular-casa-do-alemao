7b64869bb95769113962d110d6451dceb1952310
Paulo Henrique <ph.1928@hotmail.com>
2024-09-13 16:34:25 -0300

    refactor: adjust comment section to recieve article id

--- src/app/components/comment-section/comment-section.component.ts
+++ src/app/components/comment-section/comment-section.component.ts
@@ -2,14 +2,16 @@ import { Component, Input } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { Comment } from '../../models/comment';
 import { CommentComponent } from '../comment/comment.component';
+import { CommentFormComponent } from "../comment-form/comment-form.component";
 
 @Component({
   selector: 'app-comment-section',
   standalone: true,
-  imports: [HeaderComponent, CommentComponent],
+  imports: [HeaderComponent, CommentComponent, CommentFormComponent],
   templateUrl: './comment-section.component.html',
   styleUrl: './comment-section.component.scss',
 })
 export class CommentSectionComponent {
   @Input({ required: true }) comments: Comment[] | null = [];
+  @Input({required: true}) articleId: string | undefined = '';
 }
