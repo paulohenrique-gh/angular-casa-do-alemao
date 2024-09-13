66c6c5c56283bf2f1c30ebb7bd98863fa0154196
Paulo Henrique <ph.1928@hotmail.com>
2024-09-13 12:04:38 -0300

    feat: add delete event to article card component

--- src/app/components/article-card/article-card.component.ts
+++ src/app/components/article-card/article-card.component.ts
@@ -1,20 +1,37 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Article } from '../../models/article';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { AuthService } from '../../services/auth.service';
+import { UserDTO } from '../../models/user-dto';
 
 @Component({
   selector: 'app-article-card',
   standalone: true,
-  imports: [RouterModule, CommonModule],
+  imports: [RouterModule, CommonModule, FontAwesomeModule],
   templateUrl: './article-card.component.html',
   styleUrl: './article-card.component.scss'
 })
-export class ArticleCardComponent {
+export class ArticleCardComponent implements OnInit {
   @Input({required: true}) article!: Article;
+  @Output() clickDelete = new EventEmitter<string>();
+  deleteIcon = faTrash
+  currentUser: UserDTO | undefined;
+
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {
+    this.currentUser = this.authService.getCurrentUser();
+  }
 
   loadDefaultImage(event: Event): void {
     const image = event.target as HTMLImageElement;
     image.src = 'https://img.freepik.com/fotos-gratis/uma-pequena-bandeira-da-alemanha-na-cidade-borrada_485709-15.jpg';
+  }
+
+  initDelete(): void {
+    this.clickDelete.emit();
   }
 }
