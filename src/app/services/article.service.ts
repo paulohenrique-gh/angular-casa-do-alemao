7e97689ffefd2bada44b91011c8f75fe88651070
Paulo Henrique <ph.1928@hotmail.com>
2024-09-13 16:32:33 -0300

    refactor: move getComments to CommentService

--- src/app/services/article.service.ts
+++ src/app/services/article.service.ts
@@ -1,15 +1,13 @@
 import { Injectable } from '@angular/core';
-import { Observable, tap } from 'rxjs';
+import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { Article } from '../models/article';
-import { Comment } from '../models/comment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ArticleService {
   private articlesBaseUrl = 'http://localhost:3000/articles';
-  private commentsBaseUrl = 'http://localhost:3000/comments';
 
   constructor(private httpClient: HttpClient) {}
 
@@ -19,16 +17,6 @@ export class ArticleService {
 
   getArticleById(articleId: string): Observable<Article> {
     return this.httpClient.get<Article>(`${this.articlesBaseUrl}/${articleId}`);
-  }
-
-  getArticleComments(articleId: string): Observable<Comment[]> {
-    return this.httpClient
-      .get<Comment[]>(`${this.commentsBaseUrl}?articleId=${articleId}`)
-      .pipe(
-        tap((comments) =>
-          comments.sort( (a, b) => new Date(b.commentDate).getTime() - new Date(a.commentDate).getTime())
-        )
-      );
   }
 
   saveArticle(article: Article): Observable<Article> {
