283c165abac395958ca0b211e0803c8f7d44d894
Paulo Henrique <ph.1928@hotmail.com>
2024-09-13 16:32:16 -0300

    feat: create CommentService

---
+++ src/app/services/comment.service.ts
@@ -0,0 +1,32 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, tap } from 'rxjs';
+import { Comment } from '../models/comment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CommentService {
+  private commentsBaseUrl = 'http://localhost:3000/comments';
+
+  constructor(private httpClient: HttpClient) {}
+
+  getArticleComments(articleId: string): Observable<Comment[]> {
+    return this.httpClient
+      .get<Comment[]>(`${this.commentsBaseUrl}?articleId=${articleId}`)
+      .pipe(
+        tap((comments) =>
+          comments.sort((a, b) => {
+            if (a.commentDate && b.commentDate) {
+              return new Date(b.commentDate).getTime() - new Date(a.commentDate).getTime();
+            }
+            return 0;
+          })
+        )
+      );
+  }
+
+  saveComment(comment: Comment): Observable<Comment> {
+    return this.httpClient.post<Comment>(this.commentsBaseUrl, comment);
+  }
+}
