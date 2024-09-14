31c0c893eb47370cb8f6db70bc73993008d910bd
Paulo Henrique <ph.1928@hotmail.com>
2024-09-14 16:27:17 -0300

    feat: handle edit comment click

--- src/app/components/comment/comment.component.ts
+++ src/app/components/comment/comment.component.ts
@@ -3,26 +3,42 @@ import { Comment } from '../../models/comment';
 import { CommonModule } from '@angular/common';
 import { UserDTO } from '../../models/user-dto';
 import { AuthService } from '../../services/auth.service';
-import { faTrashAlt } from '@fortawesome/free-solid-svg-icons';
+import { faEdit, faTrashAlt } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { CommentFormComponent } from '../comment-form/comment-form.component';
 
 @Component({
   selector: 'app-comment',
   standalone: true,
-  imports: [CommonModule, FontAwesomeModule],
+  imports: [CommonModule, FontAwesomeModule, CommentFormComponent],
   templateUrl: './comment.component.html',
   styleUrl: './comment.component.scss'
 })
 export class CommentComponent implements OnInit {
   @Input({required: true}) comment!: Comment;
   @Output() clickDelete = new EventEmitter<Comment>();
+  @Output() commentUpdated = new EventEmitter();
   currentUser: UserDTO | undefined = undefined;
   deleteIcon = faTrashAlt;
+  editIcon = faEdit;
+  isEditEnabled = false;
 
   constructor(private authService: AuthService) {}
 
   ngOnInit(): void {
     this.currentUser = this.authService.getCurrentUser();
+  }
+
+  initEdit(): void {
+    this.isEditEnabled = true;
+  }
+
+  notifyUpdate(): void {
+    this.commentUpdated.emit();
+  }
+
+  cancelEdit(): void {
+    this.isEditEnabled = false;
   }
 
   initDelete(): void {
