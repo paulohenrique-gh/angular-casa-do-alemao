aa6a4c417c6a6d86817b80eebfbe09ea27bc9cd0
Paulo Henrique <ph.1928@hotmail.com>
2024-09-14 13:45:31 -0300

    refactor: add CommentService dependency to ArticleService

--- src/app/services/article.service.ts
+++ src/app/services/article.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { forkJoin, Observable, switchMap, tap } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { Article } from '../models/article';
+import { CommentService } from './comment.service';
+import { Comment } from '../models/comment';
 
 @Injectable({
   providedIn: 'root',
@@ -9,7 +11,10 @@ import { Article } from '../models/article';
 export class ArticleService {
   private articlesBaseUrl = 'http://localhost:3000/articles';
 
-  constructor(private httpClient: HttpClient) {}
+  constructor(
+    private httpClient: HttpClient,
+    private commentService: CommentService
+  ) {}
 
   getArticles(): Observable<Article[]> {
     return this.httpClient.get<Article[]>(this.articlesBaseUrl);
@@ -20,14 +25,27 @@ export class ArticleService {
   }
 
   saveArticle(article: Article): Observable<Article> {
-    return this.httpClient.post<Article>(this.articlesBaseUrl, article)
+    return this.httpClient.post<Article>(this.articlesBaseUrl, article);
   }
 
   updateArticle(article: Article): Observable<Article> {
-    return this.httpClient.put<Article>(`${this.articlesBaseUrl}/${article.id}`, article);
+    return this.httpClient.put<Article>(
+      `${this.articlesBaseUrl}/${article.id}`,
+      article
+    );
   }
 
   deleteArticle(articleId: string): Observable<Article> {
-    return this.httpClient.delete<Article>(`${this.articlesBaseUrl}/${articleId}`)
+    return this.httpClient.delete<Article>(`${this.articlesBaseUrl}/${articleId}`).pipe(
+      tap(article => {
+        this.commentService.getComments(article.id!).subscribe({
+          next: (commentsToDelete) => {
+            commentsToDelete.forEach(comment => {
+              this.commentService.deleteComment(comment.id!).subscribe()
+            })
+          }
+        })
+      })
+    );
   }
 }
