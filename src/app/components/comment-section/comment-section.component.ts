6bae491b534b6b97fb5ee7181aebd85d884d71c6
Paulo Henrique <ph.1928@hotmail.com>
2024-09-14 10:12:18 -0300

    refactor: move responsability of loading comments from FullArticleComponent to CommentSectionComponent

--- src/app/components/comment-section/comment-section.component.ts
+++ src/app/components/comment-section/comment-section.component.ts
@@ -1,17 +1,38 @@
-import { Component, Input } from '@angular/core';
-import { HeaderComponent } from '../header/header.component';
+import { AsyncPipe, CommonModule } from '@angular/common';
+import { Component, Input, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 import { Comment } from '../../models/comment';
+import { CommentService } from '../../services/comment.service';
+import { CommentFormComponent } from '../comment-form/comment-form.component';
 import { CommentComponent } from '../comment/comment.component';
-import { CommentFormComponent } from "../comment-form/comment-form.component";
+import { HeaderComponent } from '../header/header.component';
 
 @Component({
   selector: 'app-comment-section',
   standalone: true,
-  imports: [HeaderComponent, CommentComponent, CommentFormComponent],
+  imports: [
+    HeaderComponent,
+    CommentComponent,
+    CommentFormComponent,
+    AsyncPipe,
+    CommonModule,
+  ],
   templateUrl: './comment-section.component.html',
   styleUrl: './comment-section.component.scss',
 })
-export class CommentSectionComponent {
-  @Input({ required: true }) comments: Comment[] | null = [];
-  @Input({required: true}) articleId: string | undefined = '';
+export class CommentSectionComponent implements OnInit {
+  @Input({ required: true }) articleId: string | undefined = '';
+  comments$!: Observable<Comment[]>;
+
+  constructor(private commentService: CommentService) {}
+
+  ngOnInit(): void {
+    this.loadComments();
+  }
+
+  loadComments(): void {
+    if (this.articleId) {
+      this.comments$ = this.commentService.getArticleComments(this.articleId);
+    }
+  }
 }
--- src/app/pages/full-article/full-article.component.ts
+++ src/app/pages/full-article/full-article.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Article } from '../../models/article';
-import { Comment } from '../../models/comment';
 import { ArticleService } from '../../services/article.service';
 import { Observable } from 'rxjs';
 import { ActivatedRoute } from '@angular/router';
@@ -8,7 +7,6 @@ import { HeaderComponent } from '../../components/header/header.component';
 import { AsyncPipe, CommonModule } from '@angular/common';
 import { SplitParagraphPipe } from '../../pipes/split-paragraph.pipe';
 import { CommentSectionComponent } from '../../components/comment-section/comment-section.component';
-import { CommentService } from '../../services/comment.service';
 
 @Component({
   selector: 'app-full-article',
@@ -21,30 +19,18 @@ import { CommentService } from '../../services/comment.service';
 export class FullArticleComponent implements OnInit {
   articleId!: string | undefined;
   article$!: Observable<Article>;
-  comments$!: Observable<Comment[]>;
 
-  constructor(
-    private articleService: ArticleService,
-    private commentService: CommentService,
-    private route: ActivatedRoute
-  ) {
+  constructor(private articleService: ArticleService, private route: ActivatedRoute) {
     this.articleId = this.route.snapshot.paramMap.get('articleId') || '';
   }
 
   ngOnInit(): void {
     this.loadArticle();
-    this.loadComments();
   }
 
   private loadArticle(): void {
     if (this.articleId) {
       this.article$ = this.articleService.getArticleById(this.articleId);
-    }
-  }
-
-  private loadComments(): void {
-    if (this.articleId) {
-      this.comments$ = this.commentService.getArticleComments(this.articleId);
     }
   }
 }
