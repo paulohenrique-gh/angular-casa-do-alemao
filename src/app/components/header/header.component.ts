dcdb62573502f55dd09757d258d2b201682a0211
Paulo Henrique <ph.1928@hotmail.com>
2024-09-17 10:48:19 -0300

    chore: html formatting

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -5,16 +5,16 @@ import { Component, Input } from '@angular/core';
 /**
  * HeaderComponent is a standalone Angular component that represents the header section of the application.
  * The appropriate content to be passed is a button for user actions.
- * 
+ *
  * @selector 'app-header'
  * @standalone true
  * @imports [CommonModule]
  * @templateUrl './header.component.html'
  * @styleUrl './header.component.scss'
- * 
+ *
  * @property {string} header - The header text to be displayed. This input is required.
  * @property {'xl' | 'lg'} size - The size of the header. It can be either 'xl' (extra large) or 'lg' (large). The default value is 'xl'.
- * 
+ *
  */
 @Component({
   selector: 'app-header',
@@ -26,4 +26,6 @@ import { Component, Input } from '@angular/core';
 export class HeaderComponent {
   @Input({required: true}) header: string | undefined = '';
   @Input() size: 'xl' | 'lg' = 'xl';
+  firstLetterSizeClass = this.size === 'xl' ? 'first-letter:text-5xl' : 'first-letter:text-3xl';
+  textSizeClass = this.size === 'xl' ? 'text-3xl' : 'text-xl';
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { HeaderComponent } from "../../components/header/header.component";
+import { Country } from '../../../data';
+import { Subscription } from 'rxjs';
+import { CountryInfoService } from '../../services/country-info.service';
+import { CommonModule } from '@angular/common';
+import { SplitParagraphPipe } from '../../pipes/split-paragraph.pipe';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, CommonModule, SplitParagraphPipe],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit, OnDestroy {
+  country!: Country;
+  subscription!: Subscription;
+
+  constructor(private countryInfoService: CountryInfoService) {}
+
+  ngOnInit(): void {
+    this.subscription = this.countryInfoService.getCountry().subscribe({
+      next: (country) => this.country = country,
+      error: (error) => console.log(error)
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 }
