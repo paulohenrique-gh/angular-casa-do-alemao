62c021eb3c423de7ef24c133eabef817cb2e7455
Paulo Henrique <ph.1928@hotmail.com>
2024-09-16 23:59:27 -0300

    feat: add titles to routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -14,46 +14,57 @@ export const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
+    title: 'Início - Casa do Alemão'
   },
   {
     path: 'states',
     component: StatesComponent,
+    title: 'Estados - Casa do Alemão'
   },
   {
     path: 'gallery',
     component: GalleryComponent,
+    title: 'Galeria - Casa do Alemão'
   },
   {
     path: 'articles',
     component: ArticlesComponent,
+    title: 'Artigos - Casa do Alemão'
   },
   {
     path: 'articles/:articleId',
     component: FullArticleComponent,
+    title: 'Artigos - Casa do Alemão'
   },
   {
     path: 'signup',
     component: SignupComponent,
+    title: 'Cadastro - Casa do Alemão'
   },
   {
     path: 'login',
-    component: LoginComponent
+    component: LoginComponent,
+    title: 'Login - Casa do Alemão'
   },
   {
     path: 'my-articles',
     component: ArticlesComponent,
-    canActivate: [userArticlesGuard]
+    canActivate: [userArticlesGuard],
+    title: 'Meus Artigos - Casa do Alemão'
   },
   {
     path: 'not-found',
-    component: NotFoundComponent
+    component: NotFoundComponent,
+    title: 'Página não encontrada - Casa do Alemão'
   },
   {
     path: 'error',
-    component: ErrorComponent
+    component: ErrorComponent,
+    title: 'Erro - Casa do Alemão'
   },
   {
     path: '**',
-    component: NotFoundComponent
+    component: NotFoundComponent,
+    title: 'Página não encontrada - Casa do Alemão'
   },
 ];
