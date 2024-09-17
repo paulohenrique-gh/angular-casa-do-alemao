81ffd52299c0da7d59ca64662bb4e59afad6bfd4
Paulo Henrique <ph.1928@hotmail.com>
2024-09-17 10:50:32 -0300

    feat: add getCountry to CountryInfoService

--- src/app/services/country-info.service.ts
+++ src/app/services/country-info.service.ts
@@ -1,13 +1,18 @@
 import { Injectable } from '@angular/core';
-import { GalleryImage, galleryImages, germanStates, State } from '../../data';
+import { Country, GalleryImage, galleryImages, germanStates, germany, State } from '../../data';
 import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CountryInfoService {
+  private country: BehaviorSubject<Country> = new BehaviorSubject<Country>(germany)
   private states: BehaviorSubject<State[]> = new BehaviorSubject<State[]>(germanStates);
   private galleryImages: BehaviorSubject<GalleryImage[]> = new BehaviorSubject<GalleryImage[]>(galleryImages);
+
+  getCountry(): Observable<Country> {
+    return this.country.asObservable();
+  }
 
   getStates(): Observable<State[]> {
     return this.states.asObservable();
