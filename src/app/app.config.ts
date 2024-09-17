5d3d4ed4d901112473601c5bf9454ac7de94a3f0
Paulo Henrique <ph.1928@hotmail.com>
2024-09-17 10:50:15 -0300

    chore: configure locale

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,20 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, LOCALE_ID, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { registerLocaleData } from '@angular/common';
+import _default from '@angular/common/locales/pt';
+
+registerLocaleData(_default, 'pt')
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideHttpClient(), provideAnimationsAsync()]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideHttpClient(),
+    provideAnimationsAsync(),
+    { provide: LOCALE_ID, useValue: 'pt'}
+  ],
 };
